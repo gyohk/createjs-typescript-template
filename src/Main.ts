7167db7dd74d0bae8f1d49b43c48675c254b3069
satoru kimura <s_kimura@sa.il24.net>
2014-12-02 01:41:36 +0900

    Changed the demo.

--- src/Main.ts
+++ src/Main.ts
@@ -6,80 +6,112 @@ module app {
 
     export class Main {
         private stage:createjs.Stage;
-        private queue:createjs.LoadQueue;
+        private canvas:HTMLCanvasElement;
 
-        private image:HTMLImageElement;
-        private bitmapData:createjs.BitmapData;
-        private bd:createjs.BitmapData;
-        private bitmap:createjs.Bitmap;
-        private point:createjs.Point = new createjs.Point();
+        private squares = [];
+        private container: createjs.Container;
+        private settings: any;
+        private isChanged = true;
+        private logoBmp: createjs.Bitmap;
 
-        //preloader
-        preloader:createjs.LoadQueue;
-        manifest:Object[];
-        totalLoaded = 0;
+        private logo: HTMLImageElement;
+        private loader: HTMLImageElement;
 
         constructor(canvas_id:string) {
-            this.stage = new createjs.Stage(canvas_id);
+            this.canvas = <HTMLCanvasElement>document.getElementById(canvas_id);
+            this.stage = new createjs.Stage(this.canvas);
 
-            this.manifest = [
-                {src: "images/emitter.png", id: "emitter"}
-            ];
-
-            this.queue = new createjs.LoadQueue(false);
-            this.queue.addEventListener("progress", () => this.handleProgress());
-            this.queue.addEventListener("complete", () => this.handleComplete());
-            this.queue.addEventListener("fileload", () => this.handleFileLoad());
-            this.queue.loadManifest(this.manifest);
-        }
-
-        handleProgress(event?:createjs.Event):void {
-            //use event.loaded to get the percentage of the loading
-        }
-
-        handleComplete(event?:createjs.Event):void {
-            this.image = <HTMLImageElement>this.queue.getResult("emitter");
             this.init();
         }
 
-        handleFileLoad(event?:createjs.Event):void {
-            // do nothing.
-        }
-
         init():void {
+            this.stage.enableMouseOver(10);
+            createjs.Ticker.setFPS(60);
+            createjs.Ticker.addEventListener("tick", this.tick);
+            this.settings = {
+                max:5,
+                min:1,
+                changeEffect:this.handleChangeEffect
+            };
 
-            var board:createjs.Shape = new createjs.Shape();
-            board.graphics.beginFill("#005500");
-            board.graphics.drawRect(0, 0, 600, 400);
-            board.graphics.endFill();
-            this.stage.addChild(board);
+            var gui = new dat.GUI();
+            gui.add(this.settings, 'max', 0.1, 5);
+            gui.add(this.settings, 'min', 0.1, 2);
+            gui.add(this.settings, 'changeEffect');
 
+            this.loader = new Image();
+            this.loader.onload = this.handleLoad;
+            this.loader.src = "images/mona.jpg";
 
-            var base:createjs.Shape = new createjs.Shape();
-            base.graphics.beginFill("#996633");
-            base.graphics.drawRect(0, 400, 600, 20);
-            base.graphics.endFill();
-            this.stage.addChild(base);
-
-            this.bitmapData = new createjs.BitmapData(null, 400, 400, "rgba(0,0,0,0)");
-            this.bitmap = new createjs.Bitmap(this.bitmapData.canvas);
-            this.bitmap.x = 100;
-            this.stage.addChild(this.bitmap);
-
-            this.bd = new createjs.BitmapData(this.image, this.image.width, this.image.height);
-            this.bitmapData.pixelDissolve(this.bd, this.bd.rect, this.point, null, 400 * 400 * 0.4);
-
-            //this.stage.addChild(new createjs.Bitmap(bd.canvas));
-
-            createjs.Ticker.on("tick", () => this.tick(createjs.Event));
-            createjs.Ticker.setFPS(30);
+            this.logo = new Image();
+            this.logo.src = "images/createJS.png";
+            this.logo.onload = this.handleLogo;
         }
 
-        tick(event:createjs.Event):void {
+        handleLogo = () => {
+            this.logoBmp = new createjs.Bitmap(this.logo);
+            this.logoBmp.x = this.stage.canvas.width - this.logo.width;
+            this.logoBmp.y = this.stage.canvas.height - this.logo.height;
+            this.stage.addChild(this.logoBmp);
             this.stage.update();
-        }
+        };
+
+        handleChangeEffect = () => {
+            this.isChanged = !this.isChanged;
+        };
+
+        handleLoad = () => {
+            var width = 25;
+            var height = 25;
+            var padding = 1;
+            var cols = Math.ceil(this.loader.width / width);
+            var rows = Math.ceil(this.loader.height / height);
+            this.container = new createjs.Container();
+            this.container.cursor = "pointer";
+            var source = new createjs.Bitmap(this.loader);
+            var total = cols*rows;
+            for(var i=0;i<total;i++) {
+                var square = new createjs.Bitmap(source.cacheCanvas);
+                source.uncache();
+                var x = (width + padding) * (i%cols);
+                var y = (i/cols | 0) * (height+padding);
+                source.cache(x, y, width, height, 1);
+                square.regX = width>>1;
+                square.regY = height>>1;
+                square.x = x;
+                square.y = y;
+                this.squares.push(square);
+                this.container.addChild(square);
+            }
+            this.container.x = this.canvas.width - (cols*width)>>1;
+            this.container.y = this.canvas.height - (rows*height)>>1;
+            this.stage.addChild(this.container);
+        };
+
+        handleMouseOver = (target:createjs.Bitmap) => {
+            var maxScale = this.settings.max;
+            var minScale = this.settings.min;
+            var dx = this.container.x + (target.x - this.stage.mouseX) ;
+            var dy = this.container.y + (target.y - this.stage.mouseY) ;
+            var distance = Math.sqrt(dx*dx+dy*dy);
+            var value = (this.isChanged) ? (distance/100) : (100/distance);
+            var scale = Math.max(minScale, Math.min(value, maxScale));
+            target.scaleX = target.scaleY = scale;
+            if (this.isChanged) {
+                if (target.scaleX === maxScale) {
+                    this.container.setChildIndex(target, this.container.getNumChildren()-1);
+                }
+            }
+        };
+
+        tick = () => {
+            var l = this.squares.length;
+            for(var i=0;i<l;i++) {
+                var square = this.squares[i];
+                this.handleMouseOver(square);
+            }
+            this.stage.update();
+        };
     }
-
 }
-
 
